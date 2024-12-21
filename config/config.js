68284870046d6fb092d822629c1841e4d5fc9e06
Alisson Santos <Alisson.1007697@gmail.com>
2024-12-21 11:20:17 -0300

    Config connect

--- config/config.js
+++ config/config.js
@@ -1,19 +1,20 @@
-const { Sequelize } = require("sequelize"); // Importa Sequelize corretamente
+const Sequelize = require("sequelize");
 
-// Cria a instância do Sequelize
-const sequelize = new Sequelize('Gman', 'root', 'caio', {
-  host: 'localhost',
-  dialect: 'mysql', // Dialeto MySQL
-  port: 3000, // Adicione a porta correta caso tenha alterado
+const sequelize = new Sequelize('GmanSys', 'San_SQLLogin_1', 'eeb94y9ivn', { // Nome do banco, usuário e senha 
+  dialect: 'mssql',
+  dialectModule: require('tedious'),
+  host: 'GmanSys.mssql.somee.com',
+  port: 1433
+ 
 });
 
 const connectToDataBase = async () => {
   try {
     await sequelize.authenticate();
-    console.log(`Conexão com o banco de dados realizada com sucesso!`);
+    console.log("Conexão com o banco de dados realizada com sucesso!");
   } catch (error) {
-    console.error(`Erro ao conectar com o banco de dados: ${error}`);
+    console.error("Erro ao conectar com o banco de dados: ${error}");
   }
 };
-
+connectToDataBase()
 module.exports = { sequelize, connectToDataBase };
