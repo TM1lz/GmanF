06c8ebe5d070338b65b7b09e611d286d6a10972a
Alisson Santos <Alisson.1007697@gmail.com>
2024-12-21 14:12:28 -0300

    Cors

--- config/config.js
+++ config/config.js
@@ -1,19 +1,21 @@
 const Sequelize = require("sequelize");
 const cors = require('cors');
+const express = require('express');
+const app = express();
 
-
-app.use(cors());
+// Configuração do CORS
 app.use(cors({
-  origin: '*', // Permitir apenas esse domínio
+  origin: '*', // Permitir qualquer domínio
   methods: ['GET', 'POST'], // Especificar métodos permitidos
   allowedHeaders: ['Content-Type', 'Authorization'] // Cabeçalhos permitidos
 }));
+
+// Configuração do Sequelize
 const sequelize = new Sequelize('GmanSys', 'San_SQLLogin_1', 'eeb94y9ivn', { // Nome do banco, usuário e senha 
   dialect: 'mssql',
   dialectModule: require('tedious'),
   host: 'GmanSys.mssql.somee.com',
   port: 1433
- 
 });
 
 const connectToDataBase = async () => {
@@ -21,8 +23,11 @@ const connectToDataBase = async () => {
     await sequelize.authenticate();
     console.log("Conexão com o banco de dados realizada com sucesso!");
   } catch (error) {
-    console.error("Erro ao conectar com o banco de dados: ${error}");
+    // Corrigido para usar template literals (backticks) para interpolação de variáveis
+    console.error(`Erro ao conectar com o banco de dados: ${error}`);
   }
 };
-connectToDataBase()
+
+connectToDataBase();
+
 module.exports = { sequelize, connectToDataBase };
