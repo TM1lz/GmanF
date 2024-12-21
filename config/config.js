807032f5a45b2a1159b7ddf3195d29aa7434c2c5
Alisson Santos <Alisson.1007697@gmail.com>
2024-12-21 10:17:32 -0300

    banco de dados config

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@
 
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('Gman', 'sa', 'UserAdm123', { // Nome do banco, usuário e senha 
+const sequelize = new Sequelize('GmanSys', 'San_SQLLogin_1', 'eeb94y9ivn', { // Nome do banco, usuário e senha 
   dialect: 'mssql',
   dialectModule: require('tedious'),
-  host: 'localhost',
+  host: 'GmanSys.mssql.somee.com',
   port: 1433
  
 });
@@ -18,7 +18,7 @@ const connectToDataBase = async () => {
     console.error(`Erro ao conectar com o banco de dados: ${error}`);
   }
 };
-
+connectToDataBase()
 module.exports = { sequelize, connectToDataBase };
 
 
