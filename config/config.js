5e3531532f23501d49249305e206221cab614f0d
Alisson Santos <Alisson.1007697@gmail.com>
2024-12-21 14:07:38 -0300

    cors config

--- config/config.js
+++ config/config.js
@@ -1,5 +1,13 @@
 const Sequelize = require("sequelize");
+const cors = require('cors');
 
+
+app.use(cors());
+app.use(cors({
+  origin: '*', // Permitir apenas esse domínio
+  methods: ['GET', 'POST'], // Especificar métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'] // Cabeçalhos permitidos
+}));
 const sequelize = new Sequelize('GmanSys', 'San_SQLLogin_1', 'eeb94y9ivn', { // Nome do banco, usuário e senha 
   dialect: 'mssql',
   dialectModule: require('tedious'),
