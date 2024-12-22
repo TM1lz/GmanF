e21e872f8b10579af48d59c399c7d1bc1147dea2
TM1lz <Alisson.1007697@gmail.com>
2024-12-21 22:56:06 -0300

    Cors1

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 
-// Configuração do CORS//
+// Configuração do CORS
 app.use(cors({
   origin: '*', // Permitir qualquer domínio
   methods: ['GET', 'POST'], // Especificar métodos permitidos
@@ -14,25 +14,30 @@ app.use((req, res, next) => {
   next();
 });
 
-
 // Configuração do Sequelize
-const sequelize = new Sequelize('GmanSys', 'San_SQLLogin_1', 'eeb94y9ivn', { // Nome do banco, usuário e senha 
+const sequelize = new Sequelize('GmanSys', 'San_SQLLogin_1', 'eeb94y9ivn', { 
   dialect: 'mssql',
   dialectModule: require('tedious'),
   host: 'GmanSys.mssql.somee.com',
   port: 1433
 });
 
+// Função para conectar ao banco de dados
 const connectToDataBase = async () => {
   try {
     await sequelize.authenticate();
     console.log("Conexão com o banco de dados realizada com sucesso!");
   } catch (error) {
-    // Corrigido para usar template literals (backticks) para interpolação de variáveis
     console.error(`Erro ao conectar com o banco de dados: ${error}`);
   }
 };
 
 connectToDataBase();
 
-module.exports = { sequelize, connectToDataBase };
+// Rota de exemplo
+// Iniciar o servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
