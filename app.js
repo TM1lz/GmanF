eb27af7bc9b2f7aded7bda7e00b6d43fb161c66c
TM1lz <Alisson.1007697@gmail.com>
2025-01-18 20:23:00 -0300

    Carregando arquivo em HTML

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3080;
 const path = require("path");
-const clienteRoutes = require("./Routes/clienteRoutes.js"); // Importa as rotas de clientes
-const funcionarioRoutes = require("./Routes/funcionarioRoutes.js"); // Importa as rotas de funcionário
-const equipesDeManutencaoRoutes = require("./Routes/equipesDeManutencaoRoutes.js"); // Importa as rotas de equipes de manutenção
-const chamadaDeManutencaoRoutes = require("./Routes/chamadaDeManutencaoRoutes.js"); // Importa as rotas de chamada de manutenção
-const equipamentoRoutes = require("./Routes/equipamentosRoutes.js"); // Importa as rotas de equipamentos
+
+// Importando as rotas
+const clienteRoutes = require("./Routes/clienteRoutes.js"); 
+const funcionarioRoutes = require("./Routes/funcionarioRoutes.js");
+const equipesDeManutencaoRoutes = require("./Routes/equipesDeManutencaoRoutes.js");
+const chamadaDeManutencaoRoutes = require("./Routes/chamadaDeManutencaoRoutes.js");
+const equipamentoRoutes = require("./Routes/equipamentosRoutes.js");
+
+// Caminho para a página HTML
+const htmlPage = path.join(__dirname, "views", "index.html");
 
 // Conectar ao banco de dados
 const { connectToDataBase } = require("./config/config.js");
@@ -14,16 +19,21 @@ connectToDataBase();
 
 // Configuração do Express
 app.use(express.json()); // Para lidar com requisições JSON
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, "public"))); // Pasta para arquivos estáticos
 
-// Usar as rotas
-app.use("/api", clienteRoutes); // Prefixo "api" para todas as rotas de clientes
-app.use("/api", funcionarioRoutes); // Prefixo "api" para todas as rotas de funcionários
-app.use("/api", equipesDeManutencaoRoutes); // Prefixo "api" para todas as rotas de equipes de manutenção
-app.use("/api", chamadaDeManutencaoRoutes); // Prefixo "api" para todas as rotas de chamadas de manutenção
-app.use("/api", equipamentoRoutes); // Prefixo "api" para todas as rotas de equipamentos
+// Rota para servir o arquivo HTML
+app.get("/", (req, res) => {
+  res.sendFile(htmlPage);
+});
+
+// Usar as rotas com prefixo 'api'
+app.use("/api/clientes", clienteRoutes); 
+app.use("/api/funcionarios", funcionarioRoutes);
+app.use("/api/equipes-de-manutencao", equipesDeManutencaoRoutes);
+app.use("/api/chamadas-de-manutencao", chamadaDeManutencaoRoutes);
+app.use("/api/equipamentos", equipamentoRoutes);
 
 // Iniciar o servidor
 app.listen(port, () => {
-    console.log(`Servidor ouvindo na porta ${port}`);
+  console.log(`Servidor ouvindo na porta ${port}`);
 });
